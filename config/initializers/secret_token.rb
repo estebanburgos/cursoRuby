9036e206689c51f6fcbe273ceb412d6a9a4b02fc
estebanburgosr <estebanburgos@outlook.com>
2013-10-15 07:15:46 -0500

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyPrueba::Application.config.secret_token = 'b03a86b6c76e8fb651412815f20e6986fd61587e61f45dde33837f111ca76c8a492fa242437ca79b475f11a1fa48fec35c7afdc252c8066665ea105654814012'
